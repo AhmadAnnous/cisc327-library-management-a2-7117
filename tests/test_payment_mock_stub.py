a17c4ad997011332b99679f758f658ed7beea564
Ahmad <ahmadannous3297@gmail.com>
2025-11-01 21:01:15 -0400

    finished test cases for process_payment

--- tests/test_payment_mock_stub.py
+++ tests/test_payment_mock_stub.py
@@ -6,6 +6,8 @@ from services.library_service import (
 from services.payment_service import PaymentGateway
 
 # Pay Late Fees tests
+
+# Uses stubs to give fake valid data and a mock gateway to fake process the payment. Asserts the mock was called with valid args.
 def test_succesful_payment(mocker):
     mockpaygate = Mock(spec=PaymentGateway, patron_id = "123456", amount = 1.50)
     mockpaygate.process_payment.return_value = (True, "txn_123456", "Payment of $1.5 processed successfully")
@@ -16,20 +18,54 @@ def test_succesful_payment(mocker):
 
     assert success == True
     assert "payment successful" in message.lower()
-    mockpaygate.process_payment.assert_called_once()
+    mockpaygate.process_payment.assert_called_with(patron_id="123456", amount=1.5, description="Late fees for 'book'")
 
-# def test_payment_declined_gateway(mocker):
+# Uses stubs to give fake valid data, and a mock to fake the payment gateway failing. Asserts the mock was called with valid args.
+def test_payment_declined_by_gateway(mocker):
+    mockpaygate = Mock(spec=PaymentGateway, patron_id = "123456", amount = 1.5)
+    mockpaygate.process_payment.return_value = (False, "txn_123456", "payment not successful")
+    mocker.patch('services.library_service.calculate_late_fee_for_book', return_value = {'fee_amount': 1.50,
+        'days_overdue': 3})
+    mocker.patch('services.library_service.get_book_by_id', return_value = {"book_id" : 1, "title" : "book"})
+    success, message, trans_id = pay_late_fees("123456", 1, mockpaygate)
 
+    assert success == False
+    assert "payment failed" in message.lower()
+    mockpaygate.process_payment.assert_called_with(patron_id="123456", amount=1.5, description="Late fees for 'book'")
 
-# def test_invalid_patron_id(mocker):
+# Uses a mock gateway and asserts that the gateway wasn't called since there was an invalid id.
+def test_invalid_patron_id():
+    mockpaygate = Mock(spec=PaymentGateway, patron_id = "12345", amount = 1.50)
+    success, message, trans_id = pay_late_fees("12345", 1, mockpaygate)
 
+    assert success == False
+    assert "invalid patron id" in message.lower()
+    mockpaygate.process_payment.assert_not_called()
 
-# def test_zero_late_fees(mocker):
+# Uses a stub to give 0 late fees from calculate_late_fee_for_book, and a mock gateway to check whether process_payment was called.
+def test_zero_late_fees(mocker):
+    mockpaygate = Mock(spec=PaymentGateway, patron_id = "123456", amount = 1.5)
+    mocker.patch('services.library_service.calculate_late_fee_for_book', return_value = {'fee_amount' : 0, 'days_overdue' : 0})
 
+    success, message, trans_id = pay_late_fees("123456", 1, mockpaygate)
 
-# def test_network_error_exception_handling(mocker):
+    assert success == False
+    assert "no late fee" in message.lower()
+    mockpaygate.process_payment.assert_not_called()
 
+# Uses stubs to give fake valid data so process_payment gets called, and the return value of process_payment was stubbed as an exception
+# A Mock was used to provide the stubbed process_payment.
+def test_network_error_exception_handling(mocker):
+    mockpaygate = Mock(spec=PaymentGateway, patron_id = "123456", amount =1.5)
+    mocker.patch('services.library_service.calculate_late_fee_for_book', return_value = {'fee_amount': 1.50,
+        'days_overdue': 3})
+    mocker.patch('services.library_service.get_book_by_id', return_value = {"book_id" : 1, "title" : "book"})
+    mockpaygate.process_payment.return_value = Exception
+    success, message, trans_id = pay_late_fees("123456", 1, mockpaygate)
 
+    assert success == False
+    assert "error" in message.lower()
+    mockpaygate.process_payment.assert_called_with(patron_id="123456", amount=1.5, description="Late fees for 'book'")
 
 # # Refund Late Fee Payment Tests
 
